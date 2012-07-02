dcdba12ddb12ddaab9b4544bfa5252f023735d5f
Gert Meulyzer <cel357@gmail.com>
2012-07-02 12:23:59 +0200

    added example module.

---
+++ src/h_example.erl
@@ -0,0 +1,29 @@
+%%% @author Gert Meulyzer <@G3rtm on Twitter>
+%%% @copyright (C) 2012, Gert Meulyzer
+%%% @doc
+%%% Example module implementation
+%%% @end
+%%% Created :  2 Jul 2012 by Gert Meulyzer <@G3rtm on Twitter>
+
+-module(h_example).
+-behaviour(msghandler).
+-include("../include/irc.hrl").
+-export([handle_msg/4]).
+
+%% @doc
+%% Prefix is the part that contains the nickname and host on most messages
+%% Especially PRIVMSG etc.
+%% 
+%% Command is the type of message it is. CTCP messages get converted from PRIVMSG to
+%% CTCP and CTCP_REPLY for easy matching
+%% 
+%% Args is the list of arguments that is supplied. For example the channel to which the message
+%% has been sent.
+%% 
+%% Tail is the actual message. The things people type in IRC are here.
+%% @end
+handle_msg(_Prefix, _Command, _Args, _Tail) ->
+    ok.
+
+
+
