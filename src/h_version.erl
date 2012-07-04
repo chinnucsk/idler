c6f6924c1d7f88505ac4696afe3cb6c738a7bf01
Gert Meulyzer <cel357@gmail.com>
2012-07-04 16:44:23 +0200

    added version module

---
+++ src/h_version.erl
@@ -0,0 +1,20 @@
+-module(h_version).
+-behaviour(msghandler).
+-include("../include/irc.hrl").
+-export([handle_msg/4]).
+
+%% @doc
+%% Prefix is the part that contains the nickname and host on most messages
+%% Especially PRIVMSG etc.
+%% 
+%% Command is the type of message it is. CTCP messages get converted from PRIVMSG to
+%% CTCP and CTCP_REPLY for easy matching
+%% 
+%% Args is the list of arguments that is supplied. For example the channel to which the message
+%% has been sent.
+%% 
+%% Tail is the actual message. The things people type in IRC are here.
+%% @end
+handle_msg(Prefix, "CTCP", _Args, <<"VERSION">>) ->
+    Nick = ircmsg:nick_from_prefix(Prefix),
+    connection:send_msg(self(), <<>>, <<"CTCP_REPLY">>, [Nick], <<"DingBot -Erlang version- http://bitbucket.org/gertm/idler">> ).
