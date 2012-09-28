16cf7c69fc0392d89254549f09a991473617cb63
Gert Meulyzer <cel357@gmail.com>
2012-09-28 22:54:36 +0200

    made the url reformatting in the le_handler a bit more safe

--- src/idler_le_handler.erl
+++ src/idler_le_handler.erl
@@ -36,22 +36,19 @@ handle_msg(_Prefix, <<"PRIVMSG">>, Args, <<"!doc ", Doc/binary>>) ->
     end;
     
 handle_msg(_Prefix, <<"CTCP">>, Args, <<"ACTION searches for ", _ToSearch/binary>>) ->
-    io:format("In le search handler!"),
+    io:format("In le search handler!"), 
     idler_connection:reply(self(), Args, <<"Dunno that yet!">>);
 handle_msg(_Prefix, _Command, _Args, _Tail) ->
     ok.
 
 -spec erlang_doc_url(binary()) -> binary().
 erlang_doc_url(Doc) ->
-    case binary:split(Doc,<<":">>) of
+    case binary:split(Doc, <<":">>) of
         [H|_T] -> 
             case url_exists("http://www.erlang.org/doc/man/"++binary_to_list(H)++".html") of
                 false -> none;
                 true ->
-                    ColonReplaced = binary:replace(Doc,<<":">>,<<".html#">>),
-                    UrlPart = binary:replace(ColonReplaced, <<"/">>, <<"-">>),
-                    %%    http://www.erlang.org/doc/man/lists.html#filter-2
-                    list_to_binary("http://www.erlang.org/doc/man/"++binary_to_list(UrlPart))
+                    reformat_url(Doc)
             end;
         _ -> none
     end.
@@ -60,7 +57,16 @@ erlang_doc_url(Doc) ->
 url_exists(Url) ->
     case httpc:request(Url) of
         {error, _} -> false;
-        {ok, {{_,404,_},_,_}} -> false;
+        {ok, {{_, 404, _}, _, _}} -> false;
         _ -> true
     end.
-    
+
+reformat_url(Doc) ->
+    case binary:split(Doc, <<":">>) of
+        [H|T] ->
+            case binary:split(hd(T), <<"/">>) of 
+                [H2|T2] -> iolist_to_binary([<<"http://www.erlang.org/doc/man/">>, H, <<".html#">>, H2, <<"-">>, binary:first(hd(T2))]);
+                _ -> none
+            end;
+        _ -> none
+    end.
