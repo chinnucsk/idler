066d2bf5733b41a3d9a01f01a26229c923e89f6a
Gert Meulyzer <cel357@gmail.com>
2012-09-28 23:06:57 +0200

    reverted back to privmsg for replies

--- src/idler_connection.erl
+++ src/idler_connection.erl
@@ -40,7 +40,7 @@ send_msg(Pid, Prefix, Command, Arguments, Tail) ->
     send_msg(Pid, idler_ircmsg:create(Prefix, Command, Arguments, Tail)).
 -spec reply(Pid :: pid(), Arguments :: [binary()], Tail :: binary()) -> ok.
 reply(Pid, Arguments, Tail) ->
-    send_msg(Pid, <<>>, <<"NOTICE">>, Arguments, Tail).
+    send_msg(Pid, <<>>, <<"PRIVMSG">>, Arguments, Tail).
 
 send_raw(Pid, Line) ->
     gen_server:cast(Pid, {send_raw, Line}).
